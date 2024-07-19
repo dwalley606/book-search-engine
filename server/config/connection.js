f5107f84f7b4ffbc13d14b3b05a88269e651cef7
Daniel <dwalley606@gmail.com>
2024-07-19 11:19:00 -0400

    Fixed vite config, added error handling to connection, altered deploy command

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,15 @@
-const mongoose = require('mongoose');
+require("dotenv").config();
+const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+const mongoURI =
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/googlebooks";
+mongoose
+  .connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+  });
 
 module.exports = mongoose.connection;
